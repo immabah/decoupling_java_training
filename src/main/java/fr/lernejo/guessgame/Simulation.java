185a5411bc60266bf9741cfe292e5dcec75616d0
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-03-10 21:25:04 +0100

    test

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -10,14 +10,14 @@ public class Simulation {
 
     private final Logger logger = LoggerFactory.getLogger("Simulation");
     private final Player player;
-    private long numberToGuess;
+    private long nToGuess;
 
     public Simulation(Player player) {
         this.player=player;
     }
 
-    public void initialize(long numberToGuess) {
-        this.numberToGuess = numberToGuess;
+    public void initialize(long nToGuess) {
+        this.nToGuess = nToGuess;
     }
 
     /**
@@ -26,31 +26,31 @@ public class Simulation {
     private boolean nextRound() {
         //System.out.println(" Entrez un nombre ");
         long guess = player.askNextGuess();
-        if (guess == numberToGuess) {
+        if (guess == nToGuess) {
 
             return true;
         }
-        if (guess > numberToGuess) {
-            logger.log("Le nombre est plus petit\n");
+        if (guess > nToGuess) {
+            logger.log(" Entrez un plus petit nombre \n");
             player.respond(false);
         }else{
-            logger.log("Le nombre est plus grand \n");
+            logger.log(" Entrez un plus grand nombre \n");
             player.respond(true);
         }
 
         return false;
     }
 
-    public void loopUntilPlayerSucceed(long maximumLoops) {
+    public void loopUntilPlayerSucceed(long mL) {
         boolean win = false;
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < maximumLoops; i++) {
+        long dp = System.currentTimeMillis();
+        for (int i = 0; i < mL; i++) {
             if (nextRound()) {
                 win = true;
                 break;
             }
         }
-        long time = System.currentTimeMillis() - start;
+        long time = System.currentTimeMillis() - dp;
         if(win)
         {
             logger.log(" Vous avez gagné \n");
